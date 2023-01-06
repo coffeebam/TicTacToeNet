f4018a63a9621379ca89e9fbe8e112d6ed910466
oelyaz <79798308+oelyaz@users.noreply.github.com>
2023-01-06 20:18:11 +0100

    fertig

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 spielfeld = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 cap = 9
 
+i = 0
+for n in range(3):
+    print("|", end = "")
+    for j in range(3):
+        print(spielfeld[i], end = "|")
+        i = i + 1
+    print()
+
 def ende(spieler, feld):
     kombinationen = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for i in range(8):
@@ -13,7 +21,7 @@ def ende(spieler, feld):
 
 def setzen(spieler):
     while True:
-        position = input("Spieler " + spieler + " , an welche Position willst du setzen? (1-9)\n")
+        position = input("Spieler " + spieler + ", an welche Position willst du setzen? (1-9)\n")
         position = int(position)
         if 1 <= position <= 9:
             if type(spielfeld[position - 1]) == int:
@@ -24,19 +32,28 @@ def setzen(spieler):
     cap -= 1
     if ende(spieler, position - 1):
         print("Spieler " + spieler + " hat gewonnen!")
+        drucken()
         quit()
     if cap == 0:
         print("Unentschieden!")
+        drucken()
         quit()
 
 
-while True:
+def drucken():
     i = 0
     for n in range(3):
-        print("|", end = "")
+        print("|", end="")
         for j in range(3):
-            print(spielfeld[i], end = "|")
+            if type(spielfeld[i]) == str:
+                print(spielfeld[i], end="|")
+            else:
+                print(" ", end="|")
             i = i + 1
         print()
+
+while True:
     setzen("X")
-    setzen("Y")
+    drucken()
+    setzen("O")
+    drucken()
